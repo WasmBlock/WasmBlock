7f51b5bb629b7753ad7e2d7195e80ae07bd28bfd
Richard <richard.anaya@gmail.com>
2017-12-13 21:12:30 -0600

    rename

--- wasmblock-console.js
+++ wasmblock-console.js
@@ -1,4 +1,4 @@
-WebBlock.extensions.console = function(Module){
+WasmBlock.extensions.console = function(Module){
   return {
     console_log: function(outptr) {
       let result = Module.$copyCStr(Module, outptr);
